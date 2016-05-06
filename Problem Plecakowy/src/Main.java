8424856da0ca53228c16af1388628f127b7ae9d5
Jędrzej Ostrowski <jedrzej.ostrowski@gmail.com>
2016-05-07 01:47:46 +0200

    Dodałem tablicę wektorów (wierszy) - HMS

--- Problem Plecakowy/src/Main.java
+++ Problem Plecakowy/src/Main.java
@@ -4,8 +4,8 @@ public class Main {
 	
 	static int iloscKlockow = 20; // Iloœæ ró¿nych klocków, które program wygeneruje
 	static int dolnaGranicaKlocka = 0; // Najni¿sza mo¿liwa waga klocka
-	static int gornaGranicaKlocka = 7; // Najwy¿sza mo¿liwa waga klocka
-	static int HMS = 10; // HM Size - iloœæ wektorów (kolumn)
+	static int gornaGranicaKlocka = 3; // Najwy¿sza mo¿liwa waga klocka
+	static int HMS = 10; // HM Size - iloœæ wektorów (wierszy)
 
 	public static void main(String[] args) {
 		Random rand = new Random();
@@ -13,10 +13,17 @@ public class Main {
 		
 		int r1 = rand.nextInt(100)+1;
 		int HMCR = rand.nextInt(100)+1; // Wyloswanie parametru pocz¹tkowego HMCR
+		
+		Klocek HM[][] = new Klocek[iloscKlockow][HMS]; // Tworzê tablicê wektorów(wierszy) HM.
+		for(int i=0; i<HMS; i++){
+			HM[i] = fabryka.losujWszystkieKlocki(); // Przypisujê wektor do wiersza
+		}
+		//----- Testowy fragment do zaprezentowania dzia³ania
 		Klocek klocki[] = fabryka.losujWszystkieKlocki();
 		for(int i=0; i<iloscKlockow; i++){	
 			System.out.println("Klocek numer " + (i+1) + " wa¿y " + klocki[i].getWaga() + " kg.");
 		}
+		//----
 		if (r1<= HMCR){
 			//losowanie wektoru z tablicy i ewentualne dostrojenie liczby
 		}
