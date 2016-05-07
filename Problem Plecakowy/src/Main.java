62f3905fbe5d03e41c71773275b51ae758d2874e
Jędrzej Ostrowski <jedrzej.ostrowski@gmail.com>
2016-05-07 14:12:39 +0200

    Dodałem losowanie wektora po improwizacji HMCR

--- Problem Plecakowy/src/Main.java
+++ Problem Plecakowy/src/Main.java
@@ -1,11 +1,12 @@
 import java.util.Random;
 
 public class Main {
-	
-	static int iloscKlockow = 20; // Iloœæ ró¿nych klocków, które program wygeneruje
+
+	static int iloscKlockow = 20; // Iloœæ ró¿nych klocków, które program
+									// wygeneruje
 	static int dolnaGranicaKlocka = 0; // Najni¿sza mo¿liwa waga klocka
 	static int gornaGranicaKlocka = 3; // Najwy¿sza mo¿liwa waga klocka
-	static int HMS = 10; // HM Size - iloœæ wektorów (wierszy)
+	static int HMS = 3; // HM Size - iloœæ wektorów (wierszy)
 
 	public static void main(String[] args) {
 		Random rand = new Random();
@@ -18,14 +19,28 @@ public class Main {
 		for(int i=0; i<HMS; i++){
 			HM[i] = fabryka.losujWszystkieKlocki(); // Przypisujê wektor do wiersza
 		}
-		//----- Testowy fragment do zaprezentowania dzia³ania
-		Klocek klocki[] = fabryka.losujWszystkieKlocki();
-		for(int i=0; i<iloscKlockow; i++){	
-			System.out.println("Klocek numer " + (i+1) + " wa¿y " + klocki[i].getWaga() + " kg.");
+		//----- Ca³a wylosowana tablica
+		for(int i=0; i<HMS; i++){
+			for (int j=0; j<iloscKlockow; j++){
+				System.out.print(HM[i][j].getWaga()+ "  ");
+			}
+			System.out.println();
 		}
-		//----
+		//-----------------------------------------------
 		if (r1<= HMCR){
-			//losowanie wektoru z tablicy i ewentualne dostrojenie liczby
+			Klocek wektorHMCR[] = new Klocek[iloscKlockow];
+			for(int i=0; i<iloscKlockow; i++){
+				int losowyElement = -1;
+				for (int j=0; j<HMS; j++){
+					losowyElement = rand.nextInt(HMS); // Losujê losowy indeks wiersza
+				}
+				wektorHMCR[i]=HM[losowyElement][i]; // Pierwszy element nowego wektora to losowy element z pierwszej kolumny
+			}
+			System.out.println("");
+			for(int i=0; i<iloscKlockow; i++){
+				System.out.print(wektorHMCR[i].getWaga() + "  ");
+			}
+			// Nastêpnie PAR
 		}
 		else{
 			//tworze wektor ze wzoru
