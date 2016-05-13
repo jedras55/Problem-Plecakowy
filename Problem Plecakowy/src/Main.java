74ce57226a431cb9a9c368f06b12dce5d2c1cc09
Student <Student@193.107.33.196>
2016-05-13 12:19:13 +0200

    Losowy szkolny commit

--- Problem Plecakowy/src/Main.java
+++ Problem Plecakowy/src/Main.java
@@ -2,11 +2,12 @@ import java.util.Random;
 
 public class Main {
 
-	static int iloscKlockow = 20; // Iloœæ ró¿nych klocków, które program
+	static int iloscKlockow = 10; // Iloœæ ró¿nych klocków, które program
 									// wygeneruje
 	static int dolnaGranicaKlocka = 0; // Najni¿sza mo¿liwa waga klocka
 	static int gornaGranicaKlocka = 3; // Najwy¿sza mo¿liwa waga klocka
-	static int HMS = 3; // HM Size - iloœæ wektorów (wierszy)
+	static int HMS = 5; // HM Size - iloœæ wektorów (wierszy)
+	static double pojemnoscPlecaka = 17.0; // Pojemnoœæ plecaka - ile kg klocków mo¿e pomieœciæ
 
 	public static void main(String[] args) {
 		Random rand = new Random();
@@ -14,6 +15,7 @@ public class Main {
 		
 		int r1 = rand.nextInt(100)+1;
 		int HMCR = rand.nextInt(100)+1; // Wyloswanie parametru pocz¹tkowego HMCR
+		double tablicaSumWag[] = new double[HMS+1];
 		
 		Klocek HM[][] = new Klocek[iloscKlockow][HMS]; // Tworzê tablicê wektorów(wierszy) HM.
 		for(int i=0; i<HMS; i++){
@@ -21,9 +23,14 @@ public class Main {
 		}
 		//----- Ca³a wylosowana tablica
 		for(int i=0; i<HMS; i++){
+			double waga = 0;
 			for (int j=0; j<iloscKlockow; j++){
 				System.out.print(HM[i][j].getWaga()+ "  ");
+				waga += HM[i][j].getWaga();
 			}
+			System.out.println(waga);
+			tablicaSumWag[i]=waga;
+			waga = 0;
 			System.out.println();
 		}
 		//-----------------------------------------------
@@ -42,17 +49,74 @@ public class Main {
 			}
 			int PAR = rand.nextInt(100)+1;
 			int r3 = rand.nextInt(100)+1;
-			if(r3 <= PAR){
+			if(r3 != PAR){
 				//dostrojenie wektoru
-				double wagaWektoru = 0;
+				double wagaWektora = 0;
+				double najnizszaWaga = gornaGranicaKlocka;
+				double najwyzszaWaga  = dolnaGranicaKlocka;
+				int indeksNajmniejszegoElementu = 0;
+				int indeksNajwiekszegoElementu = 0;
 				for(int i=0; i<iloscKlockow; i++){
-					wagaWektoru += wektorHMCR[i].getWaga();  // Obliczenie sumy wag wszystkich elementów wektoru
+					wagaWektora += wektorHMCR[i].getWaga();  // Obliczenie sumy wag wszystkich elementów wektoru
+					if(wektorHMCR[i].getWaga()<najnizszaWaga){
+						najnizszaWaga = wektorHMCR[i].getWaga(); // Znajduje najmniejszy element wektoru
+						indeksNajmniejszegoElementu = i;
+					}
+					if(wektorHMCR[i].getWaga()>najwyzszaWaga){
+						najwyzszaWaga = wektorHMCR[i].getWaga(); // Znajduje najwiekszy element wektoru
+						indeksNajwiekszegoElementu = i;
+					}
+				}
+				int wspolrzednaX = rand.nextInt(iloscKlockow);
+				int wspolrzednaY = rand.nextInt(HMS);
+				if(wagaWektora < pojemnoscPlecaka){
+					wektorHMCR[indeksNajmniejszegoElementu].setWaga(HM[wspolrzednaY][wspolrzednaX].getWaga());
+					System.out.println("Waga ca³ego wektora wynosi" + wagaWektora + ", a najmniejszy wynosi " + najnizszaWaga);
+				}
+				if(wagaWektora > pojemnoscPlecaka){
+					wektorHMCR[indeksNajwiekszegoElementu].setWaga(HM[wspolrzednaY][wspolrzednaX].getWaga());
+					System.out.println("Waga ca³ego wektora wynosi " + wagaWektora + ", a najwiekszy wynosi " + najwyzszaWaga);
+				}
+				
+				System.out.println("");
+				for(int i=0; i<iloscKlockow; i++){
+					System.out.print(wektorHMCR[i].getWaga() + "  ");
+				}
+				wagaWektora = 0;
+				for(int i=0; i<iloscKlockow; i++){
+					wagaWektora += wektorHMCR[i].getWaga();  // Obliczenie sumy wag wszystkich elementów wektoru
+					if(wektorHMCR[i].getWaga()<najnizszaWaga){
+						najnizszaWaga = wektorHMCR[i].getWaga(); // Znajduje najmniejszy element wektoru
+						indeksNajmniejszegoElementu = i;
+					}
+					if(wektorHMCR[i].getWaga()>najwyzszaWaga){
+						najwyzszaWaga = wektorHMCR[i].getWaga(); // Znajduje najwiekszy element wektoru
+						indeksNajwiekszegoElementu = i;
+					}
+				}
+				if(wagaWektora < pojemnoscPlecaka){
+					System.out.println("Waga ca³ego wektora wynosi " + wagaWektora);
+					tablicaSumWag[tablicaSumWag.length-1]  = wagaWektora;
+				}
+				if(wagaWektora > pojemnoscPlecaka){
+					System.out.println("Waga ca³ego wektora wynosi " + wagaWektora);
+					tablicaSumWag[tablicaSumWag.length-1]  = wagaWektora;
 				}
 			}
 		}
 		else{
-			//tworze wektor ze wzoru
+			Klocek[] losowyWektor = new Klocek[iloscKlockow];
+			losowyWektor = fabryka.losujWszystkieKlocki();
+			double wagaLosowegoWektora = 0;
+			for (int i=0; i<iloscKlockow; i++){
+				wagaLosowegoWektora += losowyWektor[i].getWaga();
+			}
+			System.out.println("Waga nowego wektora" + wagaLosowegoWektora);
+			tablicaSumWag[tablicaSumWag.length-1]  = wagaLosowegoWektora;
 		}
-		//dostrojenie tablicy
+		for(int i=0;i<HMS+1;i++){
+			System.out.println(tablicaSumWag[i]);
+		}
+		
 	}
 }
